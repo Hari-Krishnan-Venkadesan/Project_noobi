45bdd94d165c484957e9ee78a4f5f49229062dc8
Hari KR <harikrishnan152@gmail.com>
2023-01-16 11:47:05 +0530

    new code

---
+++ first_program.py
@@ -0,0 +1,15 @@
+# add function
+def addition (num1,num2):
+    return num1 + num2
+
+# sub
+def subtraction (num1,num2):
+    return num1 - num2
+
+# multiplcation
+def multiplication (num1,num2):
+    return num1 * num2
+
+#division
+def division(num1,num2):
+    return num1 / num2
